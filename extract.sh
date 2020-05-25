193b163821b11b0cca35ed03b0ef9bbda8bd5b7a
bee-rickey <bee.ricketyship@gmail.com>
2020-05-25 13:27:14 +0530

    Folder creation

--- extract.sh
+++ extract.sh
@@ -1,6 +1,6 @@
 today=`date +"%d_%b_%Y"`
-mkdir $today
+mkdir data/$today
+#curl https://covid19.nagaland.gov.in/ > x.html
+#python3 extract.py
 git add --all
-curl https://covid19.nagaland.gov.in/ > x.html
-python3 extract.py
 git commit -m "Folder creation"
