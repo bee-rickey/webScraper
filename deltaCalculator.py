80cee86cf17f38d3bc92fe14b4dcb4b322b340ab
bee-rickey <bee.ricketyship@gmail.com>
2020-05-27 20:56:09 +0530

    Folder creation

---
+++ deltaCalculator.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+class DeltaCalculator:
+	def __init__():
+		self.covidDashboardData = requests.request("get", "https://api.covid19india.org/state_district_wise.json").json()
+		self.nameMapping = {}
+	
+	def getStateDataFromSite(stateName, stateDataFromStateDashboard):
+		stateData = self.covidDashboardData[stateName]['districtData']
+		nameMapping = self.nameMapping[stateName]
+
+		for district in (stateDataFromStateDashboard):
+			districtName = nameMapping[district['districtName']] if cases['districtName'] in nameMapping else districts['districtName']
+
+			outputString = districtName + ","
+			for key, value in districtDetails.items():
+				outputString += "," + str(district[key] - stateData[districtName][key])
+			outputString = districtName + "\n"
+
+			print(outputString)
