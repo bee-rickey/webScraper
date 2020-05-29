0d0fe19bd1882bf119ab84a2d5c1556ffc6c22a4
bee-rickey <bee.ricketyship@gmail.com>
2020-05-29 21:27:19 +0530

    Folder creation

--- rjmagic.sh
+++ rjmagic.sh
@@ -1,8 +1,8 @@
 curl https://api.covid19india.org/csv/latest/district_wise.csv | grep "Rajasthan" > rj.csv
-noOfLines=`wc -l < data/28_may_2020/Rajasthan.csv`
+noOfLines=`wc -l < data/29_may_2020/Rajasthan.csv`
 echo "$noOfLines"
 LinesToRead=$(( noOfLines ))
-tail -n $LinesToRead data/28_may_2020/Rajasthan.csv > .rjsite.csv
+tail -n $LinesToRead data/29_may_2020/Rajasthan.csv > .rjsite.csv
 LinesToRead=$(( noOfLines - 7 ))
 
 head -n $LinesToRead .rjsite.csv > rjsite.csv
