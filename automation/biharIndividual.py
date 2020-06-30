fae1e72c6bb886c485a185863926ba13bae75db9
bee-rickey <bee.ricketyship@gmail.com>
2020-06-30 15:38:50 +0530

    Update biharIndividual.py

--- automation/biharIndividual.py
+++ automation/biharIndividual.py
@@ -8,5 +8,5 @@ for index, line in enumerate(testingNumbersFile):
 	outputString = ""
 	linesArray = line.split('|')[0].split(',')
 
-	gender = "F" if linesArray[1].strip == "FEMALE" else "M"
+	gender = "F" if linesArray[1].strip() == "FEMALE" else "M"
 	print("{}, {}, {}, {}, {}, {}, {}, {}".format(linesArray[2].strip(), gender, linesArray[3].strip().title(), linesArray[4].strip().title(), 'Bihar', 'BR', 1, 'Hospitalized'))
